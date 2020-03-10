95a0b7502b8c7ed9b42d0ca68bc3796e62a96038
ADINCO <david.cruz@adinco.mx>
2020-03-10 00:24:16 -0600

    Commit Files

---
+++ Adinco Mobile/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Adinco Mobile
+//
+//  Created by smps adinco  on 10/03/20.
+//  Copyright © 2020 smps adinco . All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
